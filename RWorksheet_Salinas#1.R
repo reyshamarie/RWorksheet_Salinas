65c6bde4a45d416795d8da14606be52d4c1de6ff
Reysha Marie <reyshamarie.salinas@students.isatu.edu.ph>
2023-09-27 03:18:57 +0000

    Number 1

---
+++ RWorksheet_Salinas#1.R
@@ -0,0 +1,47 @@
+
+#Vector
+age <-c (34, 28, 22, 36, 27, 18, 52, 39, 42, 29,35, 31, 27,
+22, 37, 34, 19, 20, 57, 49, 50, 37, 46, 25, 
+17, 37, 42, 53, 41,51, 35, 24, 33, 41)
+
+ 
+#dataPoints 
+length(age)
+
+
+
+#Find the reciprocal of the values for ageWrite the R code and its output
+
+ 
+reciprocal_age <- 1/age
+reciprocal_age
+
+
+#Assign also new_age <- c(age, 0, age)What happen to the new_age?
+
+new_age <- (age, 0, age) 
+assign(new_age)
+
+
+#Sort the values for age.Write the R code and its output.
+ sort(age)
+
+ 
+#Find the minimum and maximum value for age.Write the R code and its output.
+ min(age)
+ max(age)
+ 
+ 
+#Set up a vector named data, consisting of 2.4, 2.8, 2.1, 2.5, 2.4, 2.2, 2.5, 2.3,2.5, 2.3, 2.4, and 2.7. 
+ data <-c (2.4, 2.8, 2.1, 2.5, 2.4, 2.2, 2.5, 2.3,2.5, 2.3, 2.4, 2.7. )
+ 
+#Datapoints
+ length(data)
+
+ 
+
+
+
+
+
+
