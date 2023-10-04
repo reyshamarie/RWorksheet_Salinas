109600aeadc1c69768858777f58ed0ff40c21d00
Reysha <reyshamarie.salinas@students.isatu.edu.ph>
2023-10-04 12:50:57 +0000

    NOT YET READY

---
+++ RWorksheet_Salinas#3a.R
@@ -0,0 +1,24 @@
+#Based on the above vector LETTERS:
+
+print(LETTERS)
+
+#a. You need to produce a vector that contains the first 11 letters. 
+
+LETTERS [1:11] 
+
+#b. Produce a vector that contains the odd numbered letters.
+
+LETTERS[ %% 2 ==11]
+
+
+
+#c. Produce a vector that contains the vowels 
+
+LETTERS[c(1,5,9,15,21)]
+
+
+#Based on the above vector letters:  
+
+
+#e. Produce a vector that contains letters between 15 to 24 letters in lowercase. 
+
